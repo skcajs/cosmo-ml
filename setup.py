7b82a29b78046782f1508dfcb0744611e47ed0e3
skcajs <sjackson864@outlook.com>
2023-11-09 00:41:23 +0100

    Initialized the repo with cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ML for gravitational lensing',
+    author='Sam Jackson',
+    license='MIT',
+)
